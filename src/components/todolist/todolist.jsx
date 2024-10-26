7286169af20ff95a54bc2b70b3f5f45a92bd5ee8
Elvis ingati <ingatielvis632@gmail.com>
2024-10-26 13:47:23 +0300

    mapped data and styled it

--- src/components/todolist/todolist.jsx
+++ src/components/todolist/todolist.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
-import axios from "axios";
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './todolist.css';
 
 const Todolist = () => {
-  
-  const [ todos, setTodos]= useState([])
-  
+  const [todos, setTodos] = useState([]);
 
-  const fetchData= async  ()=> {
-  const response= await axios.get('https://jsonplaceholder.typicode.com/todos')
-  const data = response.data
-  setTodos(data)
-  console.log("data", data);
-  
+  const fetchData = async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    const data = response.data;
+    setTodos(data);
+    console.log("data", data);
+  };
 
-  }
-  fetchData()
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      Helloworld
+    <div className="todolist-container">
+      {todos && todos.map((todo) => (
+        <div key={todo.id} className="todo-item">
+          <h1 className="todo-title">{todo.title}</h1>
+          <p className="todo-status">{todo.completed ? 'Completed' : 'Not Completed'}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Todolist
+export default Todolist;
