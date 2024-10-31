b4cb08d30f316fd7ec91559a24eaec5d8ce78cbf
lkimwilu <lkimwilu@gmail.com>
2024-10-31 20:58:57 +0300

    updates gallery

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./gallery.css"
+import axios from 'axios'
+
 
 const Gallery = () => {
+       const[gallery, setGallery ] = useState([])
+
+       const fetchPhotos = async() => {
+        const response= await axios.get("https://jsonplaceholder.typicode.com/photos");
+        const data = response.data;
+        setGallery(data);  //setter function
+
+        console.log("Photos",response);
+       }
+
+      useEffect(() => {
+        fetchPhotos()
+      }, [])
+      
+
   return (
-    <div>Gallery</div>
+    <div className="photolist-container">
+    {gallery && gallery.map((photo) => (
+      <div key={photo.id} className="photo-item">
+        <h1 className="photo-title">{photo.title}</h1>
+        <img src={photo.url} alt="" />
+      </div>
+    ))}
+  </div>
   )
 }
 
